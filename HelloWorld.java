11ca91a3ef215bd23b9e96bbd758906e92920087
opensourse-test <83653979+opensourse-test@users.noreply.github.com>
2021-05-06 11:43:23 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("this change is use to test pr!");
 	}
 }
